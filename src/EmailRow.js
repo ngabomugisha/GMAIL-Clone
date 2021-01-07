49616436892c0720791dc74523c7ce3efcdf7156
RRdD <rd@RDs-MacBook-Pro.local>
2021-01-07 19:56:05 +0200

    finished working on mailList UI

---
+++ src/EmailRow.js
@@ -0,0 +1,36 @@
+import { IconButton, Checkbox } from '@material-ui/core';
+import React from 'react';
+import "./EmailRow.css";
+import LabelImportantOutlinedIcon from '@material-ui/icons/LabelImportant';
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
+
+
+function EmailRow({id, title, subject, description, time}) {
+    return (
+        <div className="emailRow">
+            <div className="emailRow__options">
+                <Checkbox/>
+                <IconButton>
+                    <StarBorderOutlinedIcon/>
+                </IconButton>
+                <IconButton>
+                    <LabelImportantOutlinedIcon/>
+                </IconButton>
+            </div>
+            <h3 className="emailRow__title">
+                {title}
+            </h3>
+            <div className="emailRow__message">
+                <h4>
+                {subject} - {"   "}
+                <span className="emailRow_description">
+                    {description}
+                </span>
+                </h4>
+            </div>
+            <p className="emailRow__time" >{time}</p>
+        </div>
+    )
+}
+
+export default EmailRow
